372690bd3bee97e2bd7857e67003fcfe745fd8e4
Maycow Moura <maycowmoura@gmail.com>
2021-12-12 21:14:18 -0300

    feat: added mysql connection

---
+++ BACKEND/models/db/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=products-list;charset=utf8', 'root', '');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo _json_encode([
+    'error' => $e->getMessage()
+  ]);
+}
