4accfa3ebd777bd576377cf950062c0a7363423d
Maycow Moura <maycowmoura@gmail.com>
2021-12-17 12:07:21 -0300

    fix: removed style file

--- FRONTEND/src/components/Content/index.jsx
+++ FRONTEND/src/components/Content/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './style.css';
 import api from '../../api';
 import { useMainContext } from '../../contexts/MainContext';
 import SpinnerLoader from '../SpinnerLoader';
