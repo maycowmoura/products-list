d7c48354a89f9cf0b9d6d1ac976d6bad8b19044a
Maycow Moura <maycowmoura@gmail.com>
2021-12-17 15:23:19 -0300

    fix: fixed api url

--- FRONTEND/src/api/index.jsx
+++ FRONTEND/src/api/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   timeout: 10000,
   baseURL: process.env.NODE_ENV === 'production'
-    ? '/ponto/backend'
+    ? './backend'
     : 'http://products-list'
 })
 
