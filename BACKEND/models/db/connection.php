1395edb54c082910e7f3f9833b05d4206cbfe064
Maycow Moura <maycowmoura@gmail.com>
2021-12-19 23:42:20 -0300

    refactor: renamed folder/db to store-cart

--- BACKEND/models/db/connection.php
+++ BACKEND/models/db/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-  $pdo = new PDO('mysql:host=localhost;dbname=products-list;charset=utf8', 'root', '');
+  $pdo = new PDO('mysql:host=localhost;dbname=store-cart;charset=utf8', 'root', '');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo _json_encode([
